da6b5f0beebb05dc4f7b79e65de7eae877ed9fbc
mayank_bharti <mayankbharti349@gmail.com>
2024-08-08 22:53:57 +0530

    change_port_no

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use('/api', router); 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 7000;
 
  app.listen(port, () => {
         console.log(`Server is running at port ${port}`);
